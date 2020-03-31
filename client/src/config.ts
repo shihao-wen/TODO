d1b45dd7e2e39754c36a3a7c19fd8e0ce6dd55d5
Shihao Wen <wenshihao22@gmail.com>
2020-03-31 01:36:57 -0400

    update config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '5sesf5bozh'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-f59i18it.auth0.com',            // Auth0 domain
+  clientId: 'z7SNqiSxBkO3GYsSOpkS8Ky7yqNdeVyi',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
