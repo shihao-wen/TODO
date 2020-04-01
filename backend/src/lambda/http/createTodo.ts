f1600e53a573674359d8d513020b2f1a13dc9fd2
Shihao Wen <wenshihao22@gmail.com>
2020-03-31 21:00:21 -0400

    update

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,18 @@
-import 'source-map-support/register'
-
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import {createTodo} from '../../BusinessLogic/todoItems';
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
-
   // TODO: Implement creating a new TODO item
-  return undefined
+  const item = await createTodo(event)
+
+  return {
+    statusCode: 200,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+    body: JSON.stringify({msg:"To-do Item was created",
+      item
+    })
+  }
 }
--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,15 @@
-import 'source-map-support/register'
-
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-
+import 'source-map-support/register';
+import {deleteTodo} from '../../BusinessLogic/todoItems';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-
   // TODO: Remove a TODO item by id
-  return undefined
+  const deleted = await deleteTodo(event);
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({msg:"To-do Item was deleted",deleted:deleted})
+  }
 }
